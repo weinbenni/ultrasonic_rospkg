31d4fd92646560d7ebafe1a0db5be15f0f2ad849
weinbenni <82599478+weinbenni@users.noreply.github.com>
2023-01-24 15:54:34 +0100

    Add files via upload

---
+++ src/ultrasonic_test.py
@@ -0,0 +1,105 @@
+
+import RPi.GPIO as GPIO
+import time
+import rospy
+from sensor_msgs.msg import Range
+ 
+#GPIO Mode (BOARD / BCM)
+GPIO.setmode(GPIO.BCM)
+ 
+#set GPIO Pins1
+GPIO_TRIGGER1 = 24
+GPIO_ECHO1 = 23
+#set GPIO Pins2
+GPIO_TRIGGER2 = 14
+GPIO_ECHO2 = 15
+#set GPIO Pins3
+GPIO_TRIGGER3 = 25
+GPIO_ECHO3 = 8
+#set GPIO Pins4
+GPIO_TRIGGER4 = 25
+GPIO_ECHO4 = 8
+
+ 
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER1, GPIO.OUT)
+GPIO.setup(GPIO_ECHO1, GPIO.IN)
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER2, GPIO.OUT)
+GPIO.setup(GPIO_ECHO2, GPIO.IN)
+#set GPIO direction (IN / OUT)
+GPIO.setup(GPIO_TRIGGER3, GPIO.OUT)
+GPIO.setup(GPIO_ECHO3, GPIO.IN)
+ 
+def distance(GPIO_TRIGGER, GPIO_ECHO):
+    # set Trigger to HIGH
+    GPIO.output(GPIO_TRIGGER, True)
+ 
+    # set Trigger after 0.01ms to LOW
+    time.sleep(0.00001)
+    GPIO.output(GPIO_TRIGGER, False)
+ 
+    StartTime = time.time()
+    StopTime = time.time()
+ 
+    # save StartTime
+    while GPIO.input(GPIO_ECHO) == 0:
+        StartTime = time.time()
+ 
+    # save time of arrival
+    while GPIO.input(GPIO_ECHO) == 1:
+        StopTime = time.time()
+ 
+    # time difference between start and arrival
+    TimeElapsed = StopTime - StartTime
+    # multiply with the sonic speed (34300 cm/s)
+    # and divide by 2, because there and back
+    distance = (TimeElapsed * 343) / 2
+ 
+    return distance
+ 
+def publishSensor(sensor,measRange):
+    pub = rospy.Publisher(sensor, Range, queue_size = 10)
+    rospy.init_node('ultrasonic_publisher', anonymous = True)
+    rate = rospy.Rate(6)
+    msg = Range()
+    msg.header.frame_id = sensor
+    msg.field_of_view = 50/146.0*3.14159
+    msg.radiation_type = msg.ULTRASOUND
+    msg.max_range = 4
+    msg.min_range = 0.02
+    msg.range = measRange
+    
+    rospy.loginfo(msg)
+    pub.publish(msg)
+    rate.sleep()
+
+ 
+if __name__ == '__main__':
+    try:
+        while True:
+            try:
+                rospy.get_master().getPid()
+
+                dist1 = distance(GPIO_TRIGGER1,GPIO_ECHO1)
+                publishSensor("US1", dist1 )
+                dist2 = distance(GPIO_TRIGGER2,GPIO_ECHO2)
+                publishSensor("US2", dist2 )
+                dist3 = distance(GPIO_TRIGGER3,GPIO_ECHO3)
+                publishSensor("US3", dist3 ) 
+                dist4 = distance(GPIO_TRIGGER3,GPIO_ECHO3)
+                publishSensor("US4", dist4 ) 
+                dist5 = distance(GPIO_TRIGGER3,GPIO_ECHO3)
+                publishSensor("US5", dist5 ) 
+                dist6 = distance(GPIO_TRIGGER3,GPIO_ECHO3)
+                publishSensor("US6", dist6 ) 
+            
+            except:
+                failed = True
+                print("No connection to roscore")
+
+           
+        # Reset by pressing CTRL + C
+    except KeyboardInterrupt:
+        print("Measurement stopped by User")
+        GPIO.cleanup()
